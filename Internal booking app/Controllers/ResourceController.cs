dd634b7f92709b9834245a931a632278143ac9a6
i bring joy <i bring joy@LAPTOP-QG9PV7SU>
2025-07-15 15:06:11 +0200

    i update the database and started creating the ResourceController

---
+++ Internal booking app/Controllers/ResourceController.cs
@@ -0,0 +1,20 @@
+﻿using Internal_booking_app.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internal_booking_app.Controllers
+{
+    public class ResourceController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ResourceController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var resources = context.Resources.ToList();
+            return View(resources);
+        }
+    }
+}
