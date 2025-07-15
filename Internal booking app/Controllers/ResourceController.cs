531a0278fc418cfa20f1fd379e5d9898b536c23a
i bring joy <i bring joy@LAPTOP-QG9PV7SU>
2025-07-15 18:52:01 +0200

    i did error handling for images in the ResourceController and i also added a code to save an image file

--- Internal booking app/Controllers/ResourceController.cs
+++ Internal booking app/Controllers/ResourceController.cs
@@ -1,4 +1,5 @@
-﻿using Internal_booking_app.Services;
+﻿using Internal_booking_app.Models;
+using Internal_booking_app.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Internal_booking_app.Controllers
@@ -6,10 +7,12 @@ namespace Internal_booking_app.Controllers
     public class ResourceController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
 
-        public ResourceController(ApplicationDbContext context)
+        public ResourceController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             this.context = context;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
@@ -21,5 +24,31 @@ namespace Internal_booking_app.Controllers
             
             return View();
         }
-    }
+    [HttpPost]
+    public IActionResult Create(ResourceDto resourceDto)
+    {
+      if(resourceDto.ImageFile == null)
+      {
+       ModelState.AddModelError("ImageFile", "The image file is redquired");
+      }
+      if(!ModelState.IsValid)
+      {
+                return View(resourceDto);
+      }
+      // save the image file
+
+      string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+      newFileName += Path.GetExtension(resourceDto.ImageFile!.FileName);
+
+      string imageFullPath = environment.WebRootPath + "/resources/" + newFileName;
+      using (var stream = System.IO.File.Create(imageFullPath))
+      {
+          resourceDto.ImageFile.CopyTo(stream);
+      }
+
+
+            return RedirectToAction("Index", "Resources");
+
+    } 
+  }
 }
